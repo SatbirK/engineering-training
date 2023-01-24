08418f1fb78964951fe1f51e5edce7f6be52566f
Satbir Mehta <smehta@totalwine.com>
2023-01-24 11:08:52 -0500

    TT2-18 addedEventListner

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("Engineering Training");
 const modalButton = document.getElementById("modalButton");
 console.log("modalButton : ", modalButton);
+const modalContainer = document.getElementById('modalContainer');
+
+modalButton.addEventListener("click", function () {
+  console.log("clicked button!");
+  modalContainer.classList.toggle("hidden");
+});
+
