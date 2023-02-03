4be97213b4ac170b8da234b3728931be9693dc3f
Satbir Mehta <smehta@totalwine.com>
2023-02-03 10:08:43 -0500

    TT-20 Added Arrays JS

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const modalButton = document.getElementById("modalButton");
 console.log("modalButton : ", modalButton);
 const modalContainer = document.getElementById('modalContainer');
 
+const jiraLinks = [
+  "https://totalwine.atlassian.net/browse/TT-2",
+  "https://totalwine.atlassian.net/browse/TT-16",
+  "https://totalwine.atlassian.net/browse/TT-17",
+  "https://totalwine.atlassian.net/browse/TT-18",
+  "https://totalwine.atlassian.net/browse/TT-19",
+];
+const jiraTitles = [
+  "Create a public repository under your GitHub account",
+  "Create a new script file, and import it into index.html and add a console log",
+  "JavaScript: Variables",
+  "JavaScript: Event Listeners - Add Toggle Button Inside of Modal",
+  "JavaScript: Functions - Write a function to toggle hidden class on modal",
+];
+
+console.log("jiraTitles", jiraTitles);
+console.log("jiraLinks", jiraLinks);
+
 modalButton.addEventListener("click", function () {
   console.log("clicked button!");
   modalContainer.classList.toggle("hidden");
