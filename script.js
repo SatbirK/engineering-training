dbb23a5f7dd28c93eb52050d7c21035741829aff
Satbir Mehta <smehta@totalwine.com>
2023-02-08 10:41:01 -0500

    TT-69 ArrayIteration

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const modalButton = document.getElementById("modalButton");
 console.log("modalButton : ", modalButton);
 const modalContainer = document.getElementById('modalContainer');
 
-const jiraLinks = [
-  "https://totalwine.atlassian.net/browse/TT-2",
-  "https://totalwine.atlassian.net/browse/TT-16",
-  "https://totalwine.atlassian.net/browse/TT-17",
-  "https://totalwine.atlassian.net/browse/TT-18",
-  "https://totalwine.atlassian.net/browse/TT-19",
-];
 const jiraTitles = [
   "Create a public repository under your GitHub account",
   "Create a new script file, and import it into index.html and add a console log",
@@ -17,9 +10,21 @@ const jiraTitles = [
   "JavaScript: Event Listeners - Add Toggle Button Inside of Modal",
   "JavaScript: Functions - Write a function to toggle hidden class on modal",
 ];
+const iterateJiraTitles = jiraTitles.forEach((title) => console.log(title));
 
-console.log("jiraTitles", jiraTitles);
-console.log("jiraLinks", jiraLinks);
+const jiraLinks = [
+  "https://totalwine.atlassian.net/browse/TT-2",
+  "https://totalwine.atlassian.net/browse/TT-16",
+  "https://totalwine.atlassian.net/browse/TT-17",
+  "https://totalwine.atlassian.net/browse/TT-18",
+  "https://totalwine.atlassian.net/browse/TT-19",
+];
+jiraLinks.forEach(link);
+function link(value)
+{
+  console.log(value);
+}
+
 
 modalButton.addEventListener("click", function () {
   console.log("clicked button!");
