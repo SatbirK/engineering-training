daead87c9bfcc7feff27b7c8725e4b1c2410f938
Satbir Mehta <smehta@totalwine.com>
2023-02-13 10:59:22 -0500

    TT-21 PrintJSObjectWithArray

--- script.js
+++ script.js
@@ -3,31 +3,39 @@ const modalButton = document.getElementById("modalButton");
 console.log("modalButton : ", modalButton);
 const modalContainer = document.getElementById('modalContainer');
 
-const jiraTitles = [
+
+ jiraTitles = [
   "Create a public repository under your GitHub account",
   "Create a new script file, and import it into index.html and add a console log",
   "JavaScript: Variables",
   "JavaScript: Event Listeners - Add Toggle Button Inside of Modal",
   "JavaScript: Functions - Write a function to toggle hidden class on modal",
-];
+]; 
 jiraTitles.forEach((title) => console.log(title));
 
-const jiraLinks = [
+jiraLinks = [
   "https://totalwine.atlassian.net/browse/TT-2",
   "https://totalwine.atlassian.net/browse/TT-16",
   "https://totalwine.atlassian.net/browse/TT-17",
   "https://totalwine.atlassian.net/browse/TT-18",
   "https://totalwine.atlassian.net/browse/TT-19",
-];
+]; 
 jiraLinks.forEach(link);
 function link(value)
 {
   console.log(value);
 }
 
-jiraTitles.forEach((title, idx)=> {
-  console.log('Title : ' + title + ' , ' + 'Link : ' + jiraLinks[idx]);
-});
+
+for (let i=0; i< jiraTitles.length; i++) {
+  const jiraObject = { 
+    title : jiraTitles[i],  
+    link : jiraLinks[i]
+  }
+  console.log( "jiraObject : " , jiraObject );
+}
+
+
 
 
 
